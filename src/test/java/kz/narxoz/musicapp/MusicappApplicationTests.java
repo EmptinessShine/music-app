4736eecae3b7211ef337a3fdd6c25c9b1ab5a3b2
Shine <stockholm666p@gmail.com>
2025-12-29 14:00:00 +0500

    Initial commit: project setup and configuration

---
+++ src/test/java/kz/narxoz/musicapp/MusicappApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.narxoz.musicapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
