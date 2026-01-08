d1422837eb79a6e639d37a4d98c970e31021e067
Shine <stockholm666p@gmail.com>
2026-01-08 20:11:53 +0500

    Added REST Controller for Artists

---
+++ src/main/java/kz/narxoz/musicapp/controller/ArtistController.java
@@ -0,0 +1,23 @@
+package kz.narxoz.musicapp.controller;
+import kz.narxoz.musicapp.dto.ArtistDto;
+import kz.narxoz.musicapp.service.ArtistService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/artists")
+@RequiredArgsConstructor
+public class ArtistController {
+    private final ArtistService service;
+
+    @GetMapping
+    public List<ArtistDto> getAll() {
+        return service.getAll();
+    }
+
+    @PostMapping
+    public ArtistDto create(@RequestBody ArtistDto dto) {
+        return service.create(dto);
+    }
+}
